005305391aea15a948b6857fa13608e96b66dea9
Mebson9 <mangahonorjessica@gmail.com>
2026-02-13 18:43:16 +0100

    petites modifs

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="light" style={{ colorScheme: "light" }}>
-      <head>
-        {/* Le script le plus léger (Plausible standard) */}
-        {/* 1. Le script principal (chargement asynchrone) */}
+      <body style={{ backgroundColor: "white" }}>
         <Script
           async
           src="https://plausible.io/js/pa-uLE2cMjjUniEqBVE--pha.js"
           strategy="afterInteractive"
         />
-      </head>
 
-      <body style={{ backgroundColor: "white" }}>
         <AuthProvider>
           <ConditionalLayout>
             {children}
