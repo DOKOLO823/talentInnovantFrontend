437e6c3a86a9e47dbca5b5eba9f90c2c84b4949f
Mebson9 <mangahonorjessica@gmail.com>
2026-03-28 18:00:37 +0100

    env

--- app/lib/apifile.ts
+++ app/lib/apifile.ts
@@ -1,6 +1,6 @@
 
-const apifile = "http://localhost:8000/storage";
+// const apifile = "http://localhost:8000/storage";
 
-// const apifile = "https://foichretienne.org/talent/storage/app/public";
+const apifile = "https://foichretienne.org/talent/storage/app/public";
 
 export default apifile;
