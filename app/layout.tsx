10450b143e440e015905307e6b61333daf679a5a
Mebson9 <mangahonorjessica@gmail.com>
2026-03-28 17:53:20 +0100

    plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 import ConditionalLayout from "./components/LayoutWrapper";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script";
+// import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Talent Innovant",
@@ -20,19 +20,19 @@ export default function RootLayout({
     <html lang="fr" className="light" style={{ colorScheme: "light" }}>
       <body style={{ backgroundColor: "white" }}>
         {/* 1. Le fichier JS principal de Plausible */}
-        <Script
+        {/* <Script
           src="https://plausible.io/js/pa-uLE2cMjjUniEqBVE--pha.js"
           strategy="afterInteractive"
-        />
+        /> */}
 
         {/* 2. L'initialisation que Plausible demande (le code entre les balises <script>) */}
-        <Script id="plausible-init" strategy="afterInteractive">
+        {/* <Script id="plausible-init" strategy="afterInteractive">
           {`
     window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) };
     plausible.init = plausible.init || function(i) { plausible.o = i || {} };
     plausible.init();
   `}
-        </Script>
+        </Script> */}
 
         <AuthProvider>
           <ConditionalLayout>
